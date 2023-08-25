817c6614ef69ec50f29f8b3f02ef391e20aa80d4
Schmelzberry <jelsberry96@gmail.com>
2023-08-25 09:54:48 -0700

    pass test #1 for fetching size prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 // ******Business Logic****** // 
+
 function Pizza(toppings, size) {
   this.toppings = toppings.split(" ");
   this.size = size;
   this.price = 10;
 }
+
 // prototype for checking the price of a pizza
 Pizza.prototype.fetchPrice = function () {
   return this.price;
+}
+
+// prototype for checking the size of a pizza
+Pizza.prototype.fetchSize = function () {
+  return this.size;
 } 
 
 
